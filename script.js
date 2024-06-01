1b9dca942408008a7e6c5f9af80cc560edf1777a
Akshay <103642286+Akshay1495@users.noreply.github.com>
2024-06-01 14:25:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,51 +1,23 @@
-state: "failed",
-body: () => { 
-  cy.visit("http://localhost:3000/");
-  
-  cy.get(".price").each(($a, ind) => {
-    if (ind === 0) { 
-      cy.get($a).invoke("attr", "contenteditable", "true").then($ab => { 
-        cy.get($ab).type(55, { force: true }); 
-      }); 
-    } 
-    if (ind === 1) { 
-      cy.get($a).invoke("attr", "contenteditable", "true").then($ab => { 
-        cy.get($ab).type(75, { force: true }); 
-      }); 
-    } 
-    if (ind === 2) { 
-      cy.get($a).invoke("attr", "contenteditable", "true").then($ab => { 
-        cy.get($ab).type(100, { force: true }); 
-      }); 
-    } 
-    if (ind === 3) { 
-      cy.get($a).invoke("attr", "contenteditable", "true").then($ab => { 
-        cy.get($ab).type(130, { force: true }); 
-      }); 
-    } 
-    if (ind === 4) { 
-      cy.get($a).invoke("attr", "contenteditable", "true").then($ab => { 
-        cy.get($ab).type(50, { force: true }); 
-      }); 
-    } 
-  });
-  
-  cy.get("button").click().then(() => { 
-    cy.get("#ans").contains(410); 
-  }); 
-}
+const getSumBtn = document.createElement("button");
+getSumBtn.append("Get Total Price");
+document.body.appendChild(getSumBtn);
 
+const getSum = () => {
+//Add your code here
+	const prices=document.querySelectorAll('.price')
+	let totalPrice=0
+	prices.forEach((item)=>totalPrice+=Number(item.textContent))
+	  const table=document.querySelector('table')
+	const total=document.getElementById('total')
+	if(total){
+		total.remove()
+	}
+	table.innerHTML+=`
+		 <tr id="total">
+	      <td class="item">Total</td>
+	      <td class="prices" id="ans">${totalPrice}</td>
+	    </tr>
+	`
+};
 
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    var priceElements = document.querySelectorAll('.price');
-    var totalPrice = 0;
-
-    priceElements.forEach(function(priceElement) {
-        totalPrice += parseInt(priceElement.textContent);
-    });
-
-    var totalDiv = document.getElementById('ans');
-    totalDiv.textContent = 'Total: ' + totalPrice;
-});
+getSumBtn.addEventListener("click", getSum);
